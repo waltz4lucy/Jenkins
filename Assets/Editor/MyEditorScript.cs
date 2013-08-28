3a4f4a88e4e39467f379c56964146970872e6ea2
Lucy Diamond <waltz4lucy@gmail.com>
2013-08-28 14:59:40 +0900

    change build script

--- Assets/Editor/MyEditorScript.cs
+++ Assets/Editor/MyEditorScript.cs
@@ -1,37 +1,10 @@
 using UnityEditor;
-using System.IO;
-using System.Collections;
-using UnityEngine;
-using System.Collections.Generic;
 
-class MyEditorScript {
-        static string[] SCENES = FindEnabledEditorScenes();
-
-        static string APP_NAME = "Jenkins";
-        static string TARGET_DIR = "./Build";
-
-        [MenuItem ("Custom/CI/Build Mac OS X")]
-        static void PerformMacOSXBuild ()
-        {
-                 string target_dir = APP_NAME + ".app";
-                 GenericBuild(SCENES, TARGET_DIR + "/" + target_dir, BuildTarget.StandaloneOSXIntel,BuildOptions.None);
-        }
-
-    private static string[] FindEnabledEditorScenes() {
-        List<string> EditorScenes = new List<string>();
-        foreach(EditorBuildSettingsScene scene in EditorBuildSettings.scenes) {
-            if (!scene.enabled) continue;
-            EditorScenes.Add(scene.path);
-        }
-        return EditorScenes.ToArray();
-    }
-
-        static void GenericBuild(string[] scenes, string target_dir, BuildTarget build_target, BuildOptions build_options)
-        {
-                EditorUserBuildSettings.SwitchActiveBuildTarget(build_target);
-                string res = BuildPipeline.BuildPlayer(scenes,target_dir,build_target,build_options);
-                if (res.Length > 0) {
-                        // throw new Exception("BuildPlayer failure: " + res);
-                }
-        }
+class MyEditorScript
+{
+     static void PerformBuild ()
+     {
+         string[] scenes = { "Assets/jenkins.unity" };
+         BuildPipeline.BuildPlayer(scenes, ...);
+     }
 }
